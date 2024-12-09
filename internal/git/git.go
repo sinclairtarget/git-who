a2474f6b5da0157df795e97063babb1e2c091873
Sinclair Target <sinclairtarget@gmail.com>
2024-12-08 20:18:01 -0500

    Add function to iterate over output of git log.

--- internal/git/git.go
+++ internal/git/git.go
@@ -6,6 +6,14 @@
 */
 package git
 
+import (
+	"bufio"
+	"fmt"
+	"iter"
+	"os/exec"
+	"slices"
+)
+
 // Whether we rank authors by commit, lines, or files.
 type TallyMode int
 
@@ -15,4 +23,55 @@ const (
     FilesMode
 )
 
+// Output from a Git CLI command
+type CmdOutput struct {
+	Lines iter.Seq[string]
+	Err error
+}
 
+var baseArgs = []string {
+	"log",
+	"--pretty=format:%H:%h%n%an%n%ae%n%ad%n%s",
+	"--numstat",
+	"--date=unix",
+}
+
+// Runs git log and returns an iterator over each line of the output
+func LogLines(revs []string, path string) (*CmdOutput, error) {
+	args := slices.Concat(baseArgs, revs, []string { path })
+
+	cmd := exec.Command("git", args...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("failed to open stdout pipe: %w", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return nil, fmt.Errorf("failed to start subprocess: %w", err)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	var it CmdOutput = CmdOutput {}
+
+	it.Lines = func(yield func(string) bool) {
+		for scanner.Scan() {
+			if !yield(scanner.Text()) {
+				break
+			}
+		}
+
+		err := scanner.Err()
+		if err != nil {
+			it.Err = fmt.Errorf("failed to scan stdout: %w", err)
+		}
+
+		err = cmd.Wait()
+		if err != nil && it.Err == nil {
+			// TODO: Can we log stderr as well here to help diagnose?
+			it.Err = fmt.Errorf("error after waiting for subprocess: %w", err)
+		}
+	}
+
+	return &it, nil
+}
--- table.go
+++ table.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/sinclairtarget/git-who/internal/git"
 )
 
 // The "table" subcommand summarizes the authorship history of the given
 // commits and path in a table printed to stdout.
 func table(revs []string, path string, useCsv bool) error {
 	fmt.Printf("table() revs: %v, path: %s, useCsv: %t\n", revs, path, useCsv)
+
+	output, err := git.LogLines(revs, path)
+	if err != nil {
+		return fmt.Errorf("failed to run git log: %w", err)
+	}
+
+	for l := range output.Lines {
+		fmt.Printf("%s\n", l)
+	}
+
+	if output.Err != nil {
+		return fmt.Errorf(
+			"encountered error while iterating git log output: %w",
+			output.Err,
+		)
+	}
+
 	return nil
 }
