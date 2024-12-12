da78b769ed7e9b8731b48462845b3ed65495a47e
Sinclair Target <sinclairtarget@gmail.com>
2024-12-11 21:36:15 -0500

    Handle CSV output.

--- table.go
+++ table.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"maps"
+	"os"
+	"slices"
+	"strconv"
 
 	"github.com/sinclairtarget/git-who/internal/git"
 	"github.com/sinclairtarget/git-who/internal/tally"
@@ -11,6 +16,9 @@ import (
 // commits and path in a table printed to stdout.
 func table(revs []string, path string, useCsv bool) error {
 	fmt.Printf("table() revs: %v, path: %s, useCsv: %t\n", revs, path, useCsv)
+	if useCsv == false {
+		return fmt.Errorf("generating non-csv table not yet implemented")
+	}
 
 	lines, err := git.LogLines(revs, path)
 	if err != nil {
@@ -22,8 +30,62 @@ func table(revs []string, path string, useCsv bool) error {
 		return fmt.Errorf("failed to tally commits: %w", err)
 	}
 
-	for _, tally := range tallies {
-		fmt.Println(tally)
+	if useCsv {
+		writeCsv(tallies)
+	}
+
+	return nil
+}
+
+func toRecord(t tally.Tally) []string {
+	record := make([]string, 0)
+	return append(
+		record,
+		t.AuthorName,
+		t.AuthorEmail,
+		strconv.Itoa(t.Commits),
+		strconv.Itoa(t.LinesAdded),
+		strconv.Itoa(t.LinesRemoved),
+		strconv.Itoa(t.FileCount),
+	)
+}
+
+func writeCsv(tallies map[string]tally.Tally) error {
+	sorted := slices.SortedFunc(
+		maps.Values(tallies),
+		func (a, b tally.Tally) int {
+			if a.Commits < b.Commits {
+				return -1
+			} else if a.Commits == b.Commits {
+				return 0
+			} else {
+				return 1
+			}
+		},
+	)
+
+	w := csv.NewWriter(os.Stdout)
+
+	// Write header
+	w.Write([]string {
+		"name",
+		"email",
+		"commits",
+		"lines added",
+		"lines removed",
+		"files",
+	})
+
+	for _, tally := range sorted {
+		record := toRecord(tally)
+		if err := w.Write(record); err != nil {
+			return fmt.Errorf("error writing CSV record to stdout: %w", err)
+		}
+	}
+
+	w.Flush()
+	if err := w.Error(); err != nil {
+		return fmt.Errorf("error flushing CSV writer: %w", err)
 	}
 
 	return nil
