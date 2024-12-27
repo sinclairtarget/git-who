85256dcef89f7c224f94a274bfff50c42232ca26
Sinclair Target <sinclairtarget@gmail.com>
2024-12-27 10:29:54 -0500

    Add Collect() to iterutils.

--- internal/iterutils/iterutils.go
+++ internal/iterutils/iterutils.go
@@ -1,7 +1,10 @@
 // Iterator helpers
 package iterutils
 
-import "iter"
+import (
+	"fmt"
+	"iter"
+)
 
 // Turns a Seq into a Seq2 where the second element is always nil
 func WithoutErrors[V any](seq iter.Seq[V]) iter.Seq2[V, error] {
@@ -12,4 +15,17 @@ func WithoutErrors[V any](seq iter.Seq[V]) iter.Seq2[V, error] {
 			}
 		}
 	}
+}
+
+// Turns a seq into a slice
+func Collect[V any](seq iter.Seq2[V, error]) ([]V, error) {
+	s := []V{}
+	for v, err := range seq {
+		if err != nil {
+			return nil, fmt.Errorf("error collecting sequence: %w", err)
+		}
+		s = append(s, v)
+	}
+
+	return s, nil
 }
