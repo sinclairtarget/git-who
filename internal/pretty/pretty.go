6cb8dfd62e90cc82796ab6e27c1a8215735c0529
Sinclair Target <sinclairtarget@gmail.com>
2025-01-26 08:55:18 -0500

    Add comment with explanation of what 'dynamic' means.

--- internal/pretty/pretty.go
+++ internal/pretty/pretty.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/term"
 )
 
+// Allow backspacing and replacing output for e.g. progress indicator?
 func AllowDynamic(f *os.File) bool {
 	return term.IsTerminal(int(f.Fd()))
 }
